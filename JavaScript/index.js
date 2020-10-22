9efcc3d5612c18d09ced099c3b119df31cf36bc1
Leonardo Ibarra <ibarrapalacios@gmail.com>
2020-10-22 12:34:36 -0500

    Inheritance with JavaScript

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,3 +1,7 @@
 var car = new Car("AW456", new Account("Veronica Herrera", "VER25"));
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new UberX("AW256", new Account("Andrea Ferran", "ANDA765"), "Chevrolet", "Spark");
+uberX.passenger = 4;
+uberX.printDataCar();
