95a0beafe95c2ec1c9f9b2309c5b129bdc13c27a
Leonardo Ibarra <ibarrapalacios@gmail.com>
2020-10-24 21:50:13 -0500

    polymorphism and encapsulation

--- Java/UberVan.java
+++ Java/UberVan.java
@@ -6,11 +6,29 @@ public class UberVan extends Car
     Map<String, Map<String, Integer>> typeCarAccepted;
     ArrayList<String> seatsMaterial;
 
-    public UberVan(String license, Account driver, 
-    Map<String, Map<String, Integer>> typeCarAccepted, ArrayList<String> seatsMaterial)
+    // public UberVan(String license, Account driver, 
+    // Map<String, Map<String, Integer>> typeCarAccepted, ArrayList<String> seatsMaterial)
+    // {
+    //     super(license, driver);
+    //     this.typeCarAccepted = typeCarAccepted;
+    //     this.seatsMaterial = seatsMaterial;
+    // }
+
+    public UberVan(String license, Account driver)
     {
         super(license, driver);
-        this.typeCarAccepted = typeCarAccepted;
-        this.seatsMaterial = seatsMaterial;
+    }
+
+    @Override
+    public void setPassenger(Integer passenger)
+    {
+        if(passenger == 6)
+        {
+            this.passenger = passenger;
+        }
+        else
+        {
+            System.out.println("You need to assign 4 passengers");
+        }
     }
 }
